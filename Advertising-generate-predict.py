4fc48c5c3d25a0f6103310dae622aed32968ed70
roseainashazleen <154338276+roseainashazleen@users.noreply.github.com>
2023-12-21 14:32:47 +0800

    Add files via upload

---
+++ Advertising-generate-predict.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+st.write("# Advertising Prediction App")
+st.write("This app predicts the **Advertising** sales!")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    TV = st.sidebar.slider('TV', 0.70, 296.4, 250.0)
+    Radio = st.sidebar.slider('Radio', 0.0, 296.4, 50.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 100.0)
+
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper,}
+
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+loaded_model = pickle.load(open("Sales Advertising.h5", "rb")) #rb: read binary
+pred=loaded_model.predict(df)
+
+st.subheader('Sales')
+st.write(df)
